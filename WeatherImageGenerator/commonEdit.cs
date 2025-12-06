daf5036a7e8ed373e637dc4b8e7f510b441040b1
Vincent Leclair <vleclair90@gmail.com>
2025-12-06 14:37:35 -0500

    fixed strings and refresh time to 10 minutes

--- WeatherImageGenerator/commonEdit.cs
+++ WeatherImageGenerator/commonEdit.cs
@@ -2,16 +2,16 @@ namespace CommonStrings
 {
     public static class CommonSettings
     {
-        public const string LOCATION0 = "Montreal";
+        public const string LOCATION0 = "Montréal";
         public const string LOCATION1 = "Saint-Rémi";
-        public const string LOCATION2 = "Quebec City";
+        public const string LOCATION2 = "Québec";
         public const string LOCATION3 = "Amos";
         public const string LOCATION4 = "Sherbrooke";
         public const string LOCATION5 = "Trois-Rivières";
         public const string LOCATION6 = "Gatineau";
 
 
-        public const string REFRESHTIME = "5"; // in minutes
+        public const string REFRESHTIME = "10"; // in minutes
 
 
 
