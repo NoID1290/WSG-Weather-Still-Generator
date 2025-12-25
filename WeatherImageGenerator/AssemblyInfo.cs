67f9f02370cf5fd4a8349b4bf6012512fbf81683
Vincent Leclair <vleclair90@gmail.com>
2025-12-25 15:35:47 -0500

    Update assembly version to v0.6.10.1225

--- WeatherImageGenerator/AssemblyInfo.cs
+++ WeatherImageGenerator/AssemblyInfo.cs
@@ -16,9 +16,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.6.9.1225")]
-[assembly: AssemblyFileVersion("0.6.9.1225")]
-[assembly: AssemblyInformationalVersion("0.6.9.1225")]
+[assembly: AssemblyVersion("0.6.10.1225")]
+[assembly: AssemblyFileVersion("0.6.10.1225")]
+[assembly: AssemblyInformationalVersion("0.6.10.1225")]
+
 
 
 
