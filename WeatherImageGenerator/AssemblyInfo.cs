dad20bb8887cf584d955f5f1dda5f38788bb569d
Vincent Leclair <vleclair90@gmail.com>
2025-12-27 14:13:10 -0500

    Update assembly version to v0.7.15.1227

--- WeatherImageGenerator/AssemblyInfo.cs
+++ WeatherImageGenerator/AssemblyInfo.cs
@@ -16,9 +16,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.7.14.1227")]
-[assembly: AssemblyFileVersion("0.7.14.1227")]
-[assembly: AssemblyInformationalVersion("0.7.14.1227")]
+[assembly: AssemblyVersion("0.7.15.1227")]
+[assembly: AssemblyFileVersion("0.7.15.1227")]
+[assembly: AssemblyInformationalVersion("0.7.15.1227")]
+
 
 
 
