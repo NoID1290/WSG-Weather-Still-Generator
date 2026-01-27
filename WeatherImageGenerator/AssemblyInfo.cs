bf3a4564b8c066ee4398023bded9493d9a4008d0
Vincent Leclair <vleclair90@gmail.com>
2026-01-27 18:31:44 -0500

    Auto-commit: Version update (v1.7.1.0127)

--- WeatherImageGenerator/AssemblyInfo.cs
+++ WeatherImageGenerator/AssemblyInfo.cs
@@ -16,9 +16,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.7.0.0126")]
-[assembly: AssemblyFileVersion("1.7.0.0126")]
-[assembly: AssemblyInformationalVersion("1.7.0.0126")]
+[assembly: AssemblyVersion("1.7.1.0127")]
+[assembly: AssemblyFileVersion("1.7.1.0127")]
+[assembly: AssemblyInformationalVersion("1.7.1.0127")]
+
 
 
 
