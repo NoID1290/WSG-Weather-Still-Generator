d16c3a11becd5abdd21dce402e3eb7155a06c453
Vincent Leclair <vleclair90@gmail.com>
2026-02-04 19:27:54 -0500

    Fixing Auto-Update failing when file is lock (v1.8.14.0204)

--- WeatherImageGenerator/AssemblyInfo.cs
+++ WeatherImageGenerator/AssemblyInfo.cs
@@ -16,9 +16,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.8.13.0204")]
-[assembly: AssemblyFileVersion("1.8.13.0204")]
-[assembly: AssemblyInformationalVersion("1.8.13.0204")]
+[assembly: AssemblyVersion("1.8.14.0204")]
+[assembly: AssemblyFileVersion("1.8.14.0204")]
+[assembly: AssemblyInformationalVersion("1.8.14.0204")]
+
 
 
 
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.8.14.0204] - 2026-02-04
+
+- Fixing Auto-Update failing when file is lock
+
 ## [1.8.13.0204] - 2026-02-04
 
 - Test
